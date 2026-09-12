402073de13f47bbef2dcc5850b98092473dbd346
Hadi Bajwa <hadibajwa@DESKTOP-6JFG2GK.localdomain>
2026-09-12 22:45:50 +0500

    northwind_queries/day1_basic_select.sql

---
+++ northwind_queries/day1_basic_select.sql
@@ -0,0 +1,13 @@
+SELECT customer_id , contact_name
+FROM customers;
+SELECT * FROM products;
+SELECT COUNT(*) FROM products;
+SELECT * FROM products
+WHERE unit_price > 20;
+SELECT * FROM products
+ORDER BY product_name;
+SELECT * FROM products
+ORDER BY product_name DESC;
+SELECT unit_price
+FROM products
+LIMIT 10;
