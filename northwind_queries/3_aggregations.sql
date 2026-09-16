d789e27b9907e1c67005bec86ed183bb9d1a0aa6
iqranoor55 <iqranoor213318@gmail.com>
2026-09-16 23:47:44 +0500

    Day 3-aggregations.sql

---
+++ northwind_queries/3_aggregations.sql
@@ -0,0 +1,28 @@
+SELECT COUNT (*) 
+FROM orders;   
+SELECT COunt(DISTINCT category_name)  
+FROM categories;
+SELECT 
+    c.category_name, 
+    ROUND(SUM(od.unit_price * od.quantity*(1-od.discount))::NUMERIC,2) AS Total_revenue
+FROM categories c
+JOIN products  p ON c.category_id=p.category_id
+JOIN order_details  od ON p.product_id=od.product_id
+GROUP BY c.category_name;
+SELECT 
+    c.customer_id, 
+    c.contact_name, 
+    COUNT(o.order_id) AS total_orders
+FROM customers c
+JOIN orders o ON c.customer_id = o.customer_id
+GROUP BY c.customer_id, c.contact_name
+HAVING COUNT(o.order_id) > 5
+ORDER BY total_orders DESC;
+SELECT 
+    c.category_name,
+    ROUND(AVG(p.unit_price)::numeric, 2) AS average_price
+FROM categories c
+JOIN products p ON c.category_id = p.category_id
+GROUP BY c.category_name
+HAVING AVG(p.unit_price) > 30
+ORDER BY average_price DESC;
