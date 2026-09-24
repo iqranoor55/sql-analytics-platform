1bd066bf975b5feccd41d191e9f050ea3cc35aa9
iqranoor55 <iqranoor213318@gmail.com>
2026-09-24 22:28:05 +0500

    add queries for maximum category price and for inactive customers

--- northwind_queries/day5_subqueries.sql
+++ northwind_queries/day5_subqueries.sql
@@ -20,3 +20,27 @@ WHERE customer_id IN (
     JOIN products p ON od.product_id = p.product_id
     WHERE p.product_name = 'Chai'
 );
+-- most expensive product in each category,
+SELECT 
+    p1.category_id,
+    p1.product_name,
+    p1.unit_price
+FROM products p1
+WHERE p1.unit_price = (
+    SELECT MAX(p2.unit_price)
+    FROM products p2
+    WHERE p2.category_id = p1.category_id
+)
+ORDER BY p1.category_id;
+--customers who never placed an order
+SELECT 
+    customer_id, 
+    contact_name
+FROM customers
+WHERE customer_id NOT IN (
+    SELECT DISTINCT customer_id 
+    FROM orders 
+    WHERE customer_id IS NOT NULL
+);
+
+ 
